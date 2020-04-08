4c2face92854b5c297050ea94ace99029dea91aa
Dennis <nndenninn@gmail.com>
2020-04-08 21:11:54 +0300

    Initial Commit

---
+++ ToDoRealm-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoRealm-iOS13
+//
+//  Created by Dennis Nesanoff on 08.04.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
